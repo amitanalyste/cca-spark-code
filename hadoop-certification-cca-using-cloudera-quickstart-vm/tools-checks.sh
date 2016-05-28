a4d06d76d2697bdb21db92706d1cf982891fe2eb
mamatucci <mario.amatucci@thinkbiganalytics.com>
2016-05-28 14:25:59 +0200

    tools-check

---
+++ hadoop-certification-cca-using-cloudera-quickstart-vm/tools-checks.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+sqoop version
+hive --version
+
+touch testing
+hadoop fs -put testing /user/cloudera/
+hadoop fs -ls /user/cloudera/*
+rm testing
+hadoop fs -rm   /user/cloudera/testing
+
+spark-shell --version
+impala-shell --version
