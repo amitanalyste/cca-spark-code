38e3ea8eabbdd05faaca180132af5990c1f6e700
obar1 <mamatucci@gmail.com>
2017-03-12 10:30:05 -0700

    + reset and import all

--- 01sqoop/practice.sh
+++ 01sqoop/practice.sh
@@ -2,22 +2,26 @@
 ### obar1
 
 # using optionss-file 
-sqoop --options-file sqoop.opt --table departments --target-dir 001  &>tmp.log
+sqoop import --options-file sqoop.opt --table departments --target-dir 001  &>tmp.log
 
 # columsn
-sqoop --options-file sqoop.opt --table orders --target-dir 002  --columns  order_id,order_date  &>tmp.log
+sqoop import --options-file sqoop.opt --table orders --target-dir 002  --columns  order_id,order_date  &>tmp.log
 
 #where on fields
-sqoop --options-file sqoop.opt --table orders --target-dir 003 where "order_id > 100"  &>tmp.log
+sqoop import --options-file sqoop.opt --table orders --target-dir 003 where "order_id > 100"  &>tmp.log
 
 # free query and split by
-sqoop --options-file sqoop.opt  --query "select  * from orders where \$CONDITIONS"  --target-dir 004 --split-by order.id   &>tmp.log
+sqoop import --options-file sqoop.opt  --query "select  * from orders where \$CONDITIONS"  --target-dir 004 --split-by order.id   &>tmp.log
 
 # adding my where conditions
-sqoop --options-file sqoop.opt   --query " select  * from orders where order_id>1000 and \$CONDITIONS"   --target-dir 005 --split-by order.id  &>tmp.log
+sqoop import --options-file sqoop.opt   --query " select  * from orders where order_id>1000 and \$CONDITIONS"   --target-dir 005 --split-by order.id  &>tmp.log
 
 #validate the copy
-sqoop --options-file sqoop.opt   --table orders   --target-dir 006 --validate 
+sqoop import --options-file sqoop.opt   --table orders   --target-dir 006 --validate 
 
 #append same folder
-sqoop --options-file sqoop.opt --table departments --target-dir 001  --append
+sqoop import --options-file sqoop.opt --table departments --target-dir 001  --append
+
+#import all
+sqoop import-all-tables --options-file sqoop.opt --hive-import
+
