22de121047d49e798ea7b6daea2c696dd783a3e9
obar1 <mamatucci@gmail.com>
2017-03-13 03:45:21 -0700

    sqoop export merge

--- 01sqoop/practice.sh
+++ 01sqoop/practice.sh
@@ -33,8 +33,8 @@ hdfs dfs -du -s -h /user/hive/w*/or*
 sqoop import --options-file sqoop.opt --table orders --hive-import --compress --compression-codec org.apache.hadoop.io.compress.SnappyCodec --hive-overwrite
 
 #def database
-hive -e "CREATE DATABASE IF NOT EXISTS retail_stage" 
-sqoop import --options-file sqoop.opt --table orders --hive-import --compress --compression-codec org.apache.hadoop.io.compress.SnappyCodec --hive-database retail_stage
+hive -e "CREATE DATABASE IF NOT EXISTS retail_db" 
+sqoop import --options-file sqoop.opt --table orders --hive-import --compress --compression-codec org.apache.hadoop.io.compress.SnappyCodec --hive-database retail_db
 
 #as parquet
 sqoop import --options-file sqoop.opt --table orders -as-parquetfile --warehouse-dir /user/ma186082/sqoop_import
@@ -47,19 +47,32 @@ sqoop import --options-file sqoop.opt --table orders --boundary-query "select 1,
 
 # fields delimited and lines terminated by
 hive -f departments.hql
-sqoop import --options-file sqoop.opt --table departments --fields-terminated-by '|' --lines-terminated-by '\n' --hive-database retail_stage --hive-import --hive-table departments
+sqoop import --options-file sqoop.opt --table departments --fields-terminated-by '|' --lines-terminated-by '\n' --hive-database retail_db --hive-import --hive-table departments
 
 ## manual append
 # initial data
-`sqoop import --options-file sqoop.opt --table orders --hive-import -hive-table retail_stage.orders -where "order_id< 1000"
+`sqoop import --options-file sqoop.opt --table orders --hive-import -hive-table retail_db.orders -where "order_id< 1000"
 # adding the data with append modality
 # adding new data in append
-sqoop import --options-file sqoop.opt --table orders --hive-import -hive-table retail_stage.orders --check-column order_id --incremental append --last-value 1000
+sqoop import --options-file sqoop.opt --table orders --hive-import -hive-table retail_db.orders --check-column order_id --incremental append --last-value 1000
 sqoop eval --options-file sqoop.opt --query "select max(order_id) from orders"
-hive -e "select max(order_id) from retail_stage.orders"
+hive -e "select max(order_id) from retail_db.orders"
 
 ## export
 #create a table holder to which export data from hive
 sqoop eval --options-file sqoop.opt --query "create table orders_export as select * from retail_db.orders where 1=0"
-sqoop export --options-file sqoop.opt --table orders_export --export-dir /user/hive/warehouse/retail_stage.db/orders --batch
+sqoop export --options-file sqoop.opt --table orders_export --export-dir /user/hive/warehouse/retail_db.db/orders --batch
+
+# export with upsert
+#create a table_holder to which export data from hive
+
+sqoop import --options-file sqoop.opt --table departments --hive-import --create-hive-table  --hive-database retail_db
+       
+sqoop eval --options-file sqoop.opt --query "create table retail_db.departments_export as select * from retail_db.departments where 1=0"
+
+hdfs dfs -cat /user/hive/warehouse/r*/dep*/* | head  > departments_export.001
+vi departments_export.001 
+hdfs dfs -put departments_export.001  /user/hive/warehouse/retail_db.db/departments/
+
+sqoop export --options-file sqoop.opt --table departments_export --export-dir /user/hive/warehouse/retail_db.db/departments --update-key department_id --update-mode allowinsert 
 
--- 01sqoop/reset.sh
+++ 01sqoop/reset.sh
@@ -2,6 +2,6 @@
 hdfs dfs -rm -skipTrash -R /user/ma186082/*
 hdfs dfs -rm -skipTrash -R /user/hive/warehouse/*
 
-hive -e "drop database retail_stage cascade"
-hive -e "create database retail_stage"
+hive -e "drop database retail_db cascade"
+hive -e "create database retail_db"
 
